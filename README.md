7600336be45756650208c90672c873ff84e736d9
tuxayo <victor@tuxayo.net>
2014-04-03 19:29:32 +0200

    First version initial import: 0.2.0

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
 twitMiner-tweet-collector.py
 ============================
 
-Application used to extract data from twitter to a csv file that will be used for data mining classes
+Application used to extract data from twitter to a csv file that will be used in a data mining project for university:
+[https://github.com/IUTInfoAix/twitMiner/blob/master/Sujet.md](https://github.com/IUTInfoAix/twitMiner/blob/master/Sujet.md)
+
+## Dependencies installation
+sudo pip install twitter
+
+OR
+
+sudo easy_install twitter
+
+## Launch for the first time
+- Place your credentials and app name in ./twitter\_credentials.txt (see ./twitter\_credentials.txt.example)
+- from this directory, run:
+
+python3 tweet\_collector/tweet\_collector_main.py
+
+## Version 0.2.0
+What works:
+
+- logs in to Twitter
+- get data from a user
+
+### Credits
+Inspired by [http://tutorials.iq.harvard.edu/twitter](http://tutorials.iq.harvard.edu/twitter)
+
+### License
+GPLv3
---
+++ tweet_collector/login.py
@@ -0,0 +1,47 @@
+# Python 3
+import twitter
+import sys
+
+from twitter.oauth import write_token_file, read_token_file
+from twitter.oauth_dance import oauth_dance
+
+
+def login_and_get_twitter():
+
+    # Go to http://twitter.com/apps/new to create an app and get these items
+    # See also http://dev.twitter.com/pages/oauth_single_token
+
+    CREDENTIALS_FILE = './twitter_credentials.txt'
+    TOKEN_FILE = './twitter_token.oauth'  # in the current directory
+#     try:
+    (app_name, consumer_key, consumer_secret) = read_credentials_file(CREDENTIALS_FILE)
+#     except IOError:
+#         print(TOKEN_FILE + " not found")
+    
+    try:
+        (oauth_token, oauth_token_secret) = read_token_file(TOKEN_FILE)
+        print('read token from file success')
+    except IOError as e:
+        print('read token from file failed, requesting new token')
+        (oauth_token, oauth_token_secret) = oauth_dance(app_name, consumer_key,
+                consumer_secret)
+  
+        write_token_file(TOKEN_FILE, oauth_token, oauth_token_secret)
+
+    return twitter.Twitter(domain='api.twitter.com', api_version='1.1',
+                        auth=twitter.oauth.OAuth(oauth_token, oauth_token_secret,
+                        consumer_key, consumer_secret))
+
+def read_credentials_file(filename):
+    """
+    Read a credentials file and return the app name, consumer key and consumer secret.
+    """
+    try:
+        f = open(filename)
+        return f.readline().strip(), f.readline().strip(), f.readline().strip()
+        
+    except IOError:
+        print(filename + ' not found, please create one using '
+                    'twitter_credentials.txt.example')
+        sys.exit(1)
+    
